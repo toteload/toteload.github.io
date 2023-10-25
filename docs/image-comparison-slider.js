d2a9390d4c4eb95bdeb2ba77fb683453c58b67bf
toteload <toteload@users.noreply.github.com>
2023-10-25 11:33:32 +0200

    Improve image comparison

--- docs/image-comparison-slider.js
+++ docs/image-comparison-slider.js
@@ -3,8 +3,6 @@ function registerSlider(slider) {
     let right = slider.getElementsByClassName("cmp-right")[0];
     let divider = slider.getElementsByClassName("cmp-divider")[0];
 
-    let enabled = false;
-
     function update(e) {
         let {x, y, width} = slider.getBoundingClientRect();
         let sx = Math.floor(e.clientX - x);
@@ -14,10 +12,7 @@ function registerSlider(slider) {
         divider.style.left = sx + 'px';
     }
 
-    slider.addEventListener("pointerup",    (e) => { enabled = false; });
-    slider.addEventListener("pointerleave", (e) => { enabled = false; });
-    slider.addEventListener("pointerdown",  (e) => { enabled = true; update(e); });
-    slider.addEventListener("pointermove",  (e) => { if (!enabled) { return; } update(e); });
+    slider.addEventListener("pointermove", (e) => { if (!(e.buttons & 1)) { return; } update(e); });
 }
 
 function registerSliders() {
