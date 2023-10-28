641ad1f2b809104306ed2eec0d1750c76ada4709
toteload <toteload@users.noreply.github.com>
2023-10-28 18:21:21 +0200

    fix slider not working on mobile

--- docs/image-comparison-slider.js
+++ docs/image-comparison-slider.js
@@ -7,11 +7,14 @@ function registerSlider(slider) {
         let {x, y, width} = slider.getBoundingClientRect();
         let sx = Math.floor(e.clientX - x);
 
+        console.log(width);
+
         left.style.width = sx + 'px';
         right.style.width = (width - sx) + 'px';
         divider.style.left = sx + 'px';
     }
 
+    slider.addEventListener("pointerdown", (e) => update(e));
     slider.addEventListener("pointermove", (e) => { if (!(e.buttons & 1)) { return; } update(e); });
 }
 
